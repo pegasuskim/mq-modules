51d8b6568c0e9317592fbb8ea1f88d8d34fd63ed
pegasuskim <pegasuskim@gmail.com>
2018-03-28 15:33:10 +0900

    arranged

--- topics-receive.js
+++ topics-receive.js
@@ -77,13 +77,13 @@ function consumerStart() {
     conn.createChannel(function(err, ch) {
       var ex = config.topics.exchanges;
       ch.assertExchange(ex, 'topic', {durable: false});
-      
+
       var first = config.topics.firstq;
       var second = config.topics.secondq;
 
       // queue1 createsecond and bind queue, consume !!
       ch.assertQueue('', {exclusive: true}, function(err, q) {
-        //console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
         var topics_key = config.topics.topics_key
 
         topics_key.forEach(function(key) {
@@ -100,8 +100,8 @@ function consumerStart() {
 
       // queue2 create and bind queue, consume !!
       ch.assertQueue('', {exclusive: true}, function(err, q) {
-        //console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
-        //console.log("\n");
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+        console.log("\n");
         var error_key = config.topics.error_key
         error_key.forEach(function(key) {
           //ch.bindQueue(q.queue, ex, key);
