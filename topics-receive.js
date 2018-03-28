7b0e4018f75bfa10859e2a9a7a8dc9a49016e927
pegasuskim <pegasuskim@gmail.com>
2018-03-28 16:47:33 +0900

    arranged

--- topics-receive.js
+++ topics-receive.js
@@ -83,8 +83,8 @@ function consumerStart() {
       var first = config.topics.firstq;
       var second = config.topics.secondq;
 
-      ch.deleteQueue(first, {ifUnused: true, ifEmpty:true} );
-      ch.deleteQueue(second,{ifUnused: true, ifEmpty:true} );
+      ch.deleteQueue(first, {ifUnused: false, ifEmpty:false} );
+      ch.deleteQueue(second,{ifUnused: false, ifEmpty:false} );
 
       // queue1 createsecond and bind queue, consume !!
       ch.assertQueue(first, {exclusive: false}, function(err, q) {
