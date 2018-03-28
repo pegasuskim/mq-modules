034d1cd14c3ddcd64c550f23491a8ab6b56e36e7
pegasuskim <pegasuskim@gmail.com>
2018-03-28 19:13:16 +0900

    arranged

--- topics-receive.js
+++ topics-receive.js
@@ -63,7 +63,13 @@ function consumerStart() {
     conn.on("error", function(err) {
       if (err.message !== "Connection closing") {
         console.log("[AMQP] conn error", err.message);
+        return setTimeout(function () {
+            console.log('now attempting reconnect ...');
+            consumerStart();
+        }, reconnectTimeout);        
+        
       }
+      
     });
                
     conn.on("close", function() {
